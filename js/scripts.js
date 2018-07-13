19a5eff72dc2b821319f5e9cd9ce8fd538adef05
Immanuel007 <sainabarboi@gmail.com>
2018-07-13 17:44:17 +0300

    Addedlists on index.html and used for loop in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
-$(document).ready(function()
-{
-  $("form#pingPong").submit(function(event)
-  {
-    event.preventDefault();
-    var ping = parseInt($("input#ping").val());
-    var result = pingPong(ping);
-    $("#result").append(result);
-
-  });
-
+$(document).ready(function(){
+ $("form").click(function(event){
+   event.preventDefault();
+   var number =$("#number").val();
+   var result = parseInt(number);
+   user(result);
+   my.forEach(function(result){
+   $("#outcome").append("<li>" + result + "</li>");
+ });
+});
 });
 
+// business logic
+var my=[];
+ function user(result){
+   for(index=1;index<=result;index++){
 
-var pingPong = function(ping){
-
-  if((ping%3) === 0 && (ping%5) === 0) {
-    return "pingpong";
-  }
-
-  else if ((ping%5)===0) {
-    return "pong";
-  }
-
-  else if ((ping%3)===0) {
-  return "ping";
-  }
-
-  else{
-    return ping;
-  }
-};
+ if (index % 15 === 0) {
+   my.push("pingpong");
+}
+ else if (index % 3 === 0){
+   my.push("ping");
+}
+ else if (index % 5 === 0){
+   my.push("pong");
+ }
+ else {
+   my.push(index);
+ }
+}
+}
