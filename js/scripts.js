f4affc6f4c548ffe73b7d2aade0bfdc9f53e0018
Immanuel007 <sainabarboi@gmail.com>
2018-07-16 11:55:07 +0300

    Done spacing in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@
 
 
 //user Interface logic
-$(document).ready(function(){
- $("form").click(function(event){
+$(document).ready(function()
+{
+ $("form").click(function(event)
+ {
    event.preventDefault();
    var number =$("#number").val();
    var result = parseInt(number);
    user(result);
-   my.forEach(function(result){
+   my.forEach(function(result)
+   {
+
    $("#outcome").append("<li>" + result + "</li>");
- });
-});
+   });
+  });
 });
 
 
@@ -20,20 +24,26 @@ $(document).ready(function(){
 
 // business logic
 var my=[];
- function user(result){
-   for(index=1;index<=result;index++){
+function user(result)
+ {
+   for(index=1;index<=result;index++)
+   {
 
- if (index % 15 === 0) {
-   my.push("pingpong");
-}
- else if (index % 3 === 0){
-   my.push("ping");
-}
- else if (index % 5 === 0){
-   my.push("pong");
+ if (index % 15 === 0)
+ {
+    my.push("pingpong");
  }
- else {
-   my.push(index);
+  else if (index % 3 === 0)
+  {
+    my.push("ping");
+  }
+  else if (index % 5 === 0)
+  {
+    my.push("pong");
+  }
+  else
+  {
+    my.push(index);
  }
 }
 }
